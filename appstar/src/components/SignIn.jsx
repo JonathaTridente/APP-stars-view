7c793a9667ef2a9ed6e5541dcb0404a8126d8853
JonathaTridente <jonathapietro@hotmail.com>
2022-06-26 09:21:25 -0300

    FormSignUpdate

--- appstar/src/components/SignIn.jsx
+++ appstar/src/components/SignIn.jsx
@@ -1,6 +1,6 @@
 import applogo from './assets/applogo.png'
 import './SignIn.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 
 function SignIn() {
@@ -8,35 +8,44 @@ function SignIn() {
     const [user, setUser] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [image, setImage] = useState("")
+
+    const uploadImage = async e => {
+        e.preventDefault();
+        console.log("Upload Image");
+        console.log(image);
+    }
+
 
     return (
 
         <div className="container-Main-SignIn">
-            
-            <div className="container-SignIn">
-                
-                <div className="wrap-SignIn">
-                <span>
-                            < img src={applogo} className="App-logo" alt="logo" />
-                        </span>
-                    <form className="SignIn-form">
-                      
-                        <span className="SignIn-form-tittle">Suba a bordo observador!</span>
 
-                        
-                        <div className="wrap-input">
+            <div className="container-SignIn">
+
+                <div className="WrapSign">
+
+                    <form className="SignIn-form">
+                        <span>
+                            < img src={applogo} className="ApplogoSign" alt="logo" />
+                        </span>
+
+                        <span className="SignIn-form-tittle">Suba a bordo, observador!</span>
+
+
+                        <div className="WrapInputSign">
                             <input
                                 className={user !== "" ? 'has-val input' : 'input'}
                                 type="user"
                                 value={user}
                                 onChange={e => setUser(e.target.value)}
                             />
-                            <span className="focus-input" data-placeholder="user"></span>
+                            <span className="focus-input" data-placeholder="User"></span>
                         </div>
 
 
 
-                        <div className="wrap-input">
+                        <div className="WrapInputSign">
                             <input
                                 className={email !== "" ? 'has-val input' : 'input'}
                                 type="email"
@@ -46,7 +55,7 @@ function SignIn() {
                             <span className="focus-input" data-placeholder="Email"></span>
                         </div>
 
-                        <div className="wrap-input">
+                        <div className="WrapInputSign">
                             <input
                                 className={password !== "" ? 'has-val input' : 'input'}
                                 type="password"
@@ -56,8 +65,21 @@ function SignIn() {
                             <span className="focus-input" data-placeholder="Password"></span>
                         </div>
 
+                        <div className="wrapImage">
+                            <form onSubmit={uploadImage}>
+                                <label className="txt3">Avatar: </label>
+                                <input ClassName="Imagefile"
+                                    type="file"
+                                    name="image"
+                                    onChange={(e => setImage(e.target.files[0]))}
+                                /><br /><br />
+                                <button className="ImageSave" type="submit">Save</button>
+                            </form>
+                        </div>
+
+
                         <div className="container-SignIn-form-btn">
-                            <button className="SignIn-form-btn">SignIn</button>
+                            <button className="SignIn-form-btn">Sign In</button>
                         </div>
 
                     </form>
